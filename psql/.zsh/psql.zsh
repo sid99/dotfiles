021a1e2b2dda9eb6a3e6bfd84b82b0918a314014
Nithesh Mittapally <nithesh.chandra@gmail.com>
2015-02-12 11:32:29 -0500

    Upgrades $PATH for psql to the latest Postgres.app version

--- psql/.zsh/psql.zsh
+++ psql/.zsh/psql.zsh
@@ -1,2 +1,2 @@
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
 alias psql="PAGER=less LESS='-iMSx4 -FX' psql"
